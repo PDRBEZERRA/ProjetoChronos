acae2178b9ad7a748ca28a6dff990e28597155ff
PDRBEZERRA <pedroasbezerra05@gmail.com>
2026-05-05 16:42:39 -0300

    Alterando script troca cenario

--- Assets/LanternaChronos.cs
+++ Assets/LanternaChronos.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LanternaChronos : MonoBehaviour
 {
     public GameObject cenarioAtual;
     public GameObject cenarioAntigo;
+
+    // Nova variável para receber a ação do Input System
+    public InputActionReference acaoTrocarCenario;
 
     private bool mostrandoAntigo = false;
 
@@ -13,15 +17,30 @@ public class LanternaChronos : MonoBehaviour
         cenarioAntigo.SetActive(false);
     }
 
-    void Update()
+    // O Update foi removido. Agora usamos OnEnable para "ligar" a escuta do botão
+    private void OnEnable()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (acaoTrocarCenario != null)
         {
-            AlternarTempo();
+            acaoTrocarCenario.action.Enable();
+            
+            acaoTrocarCenario.action.performed += AlternarTempo;
         }
     }
 
-    void AlternarTempo()
+    // Usamos OnDisable para "desligar" a escuta e evitar erros na memória
+    private void OnDisable()
+    {
+        if (acaoTrocarCenario != null)
+        {
+            acaoTrocarCenario.action.Disable();
+            
+            acaoTrocarCenario.action.performed -= AlternarTempo;
+        }
+    }
+
+    // A função agora recebe o CallbackContext do Input System
+    void AlternarTempo(InputAction.CallbackContext context)
     {
         mostrandoAntigo = !mostrandoAntigo;
 
